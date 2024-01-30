bf2e90313b8adad6d06be9f917f335f61244ab6d
Satyam Verma <satyamverma46@outlook.com>
2024-01-30 21:45:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export function webBg(ID, zIndex) {
     let e = document.createElement('div');
     e.id = 'webBackgroundFrame';
     e.style.cssText = 'position: fixed;width: 100%;height: 100%;z-index: 0;border: 0;padding: 0';
-    const u = 'http://127.0.0.1:443'; // https://SatyamV7.github.io/webBackground/
+    const u = 'https://SatyamV7.github.io/webBackground/'; // http://127.0.0.1:443
     if (isNaN(ID) == false & ID <= webBgData.length & zIndex !== null & isNaN(zIndex) == false) {
         var zI = zIndex;
         console.log(`webBackground Configuration:`, '\n', `Background ID:`, ID, '\n', `Background Name:`, webBgData[ID].Name, '\n', `z-index for webBackgroundFrame:`, zI);
